915f5285d0b48df0b127d365c0282b4588f73337
richard <fasanriccardo21@gmail.com>
2025-05-03 19:37:41 +0200

    build: 0.0.9

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import flutter_local_notifications
 import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
